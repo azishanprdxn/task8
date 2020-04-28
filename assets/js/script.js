bd3d00ec7407f56a80780a9997a62206b17d1ce1
Zishan Ansari <zshan.ansari07@gmail.com>
2020-04-28 17:57:53 +0530

    Optimised changeImage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // JavaScript here
-
 var images = [ // Storing images in array
   './assets/images/1.jpeg',
   './assets/images/2.jpg',
@@ -11,34 +10,20 @@ var images = [ // Storing images in array
   './assets/images/8.jpeg',
   './assets/images/9.png',
 ]
+var imagesLength = images.length;
 
 var image = document.getElementById('image');
-var counter = 1;
-
+var counter = 0;
 var timer = setInterval(changeImage, 1000);
 
 function changeImage() {
-  if (counter == 1) {
-    image.src = images[0];
-  } else if (counter == 2) {
-    image.src = images[1];
-  } else if (counter == 3) {
-    image.src = images[2];
-  } else if (counter == 4) {
-    image.src = images[3];
-  } else if (counter == 5) {
-    image.src = images[4];
-  } else if (counter == 6) {
-    image.src = images[5];
-  } else if (counter == 7) {
-    image.src = images[6];
-  } else if (counter == 8) {
-    image.src = images[7];
-  } else if (counter == 9) {
-    image.src = images[8];
-  } else {
-    counter = 1;
-    image.src = images[0];
+  for (var i = 0; i < imagesLength; i++) {
+    if (counter == i) {
+      image.src = images[i];
+    }
   }
   counter++;
+  if (counter > images.length - 1) {
+    counter = 0; // Resets the counter when all the image is traversed
+  }
 }
